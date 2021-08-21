05396c3c50d10fc94ce08044bf0c78fc70311140
KLFzx <kliffa7@gmail.com>
2021-08-21 21:35:03 +0100

    Added creation of file for hungergames plugin

--- src/main/java/com/timewars/datarm/files/CustomConfig.java
+++ src/main/java/com/timewars/datarm/files/CustomConfig.java
@@ -4,17 +4,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class CustomConfig {
 
     private static File file; //real file which we convert to customFile
+    private static File fileForHungerGames; //real file which we convert to customFile
     private static FileConfiguration customFile;
 
     public static void setup()
     {
         file = new File(Bukkit.getServer().getPluginManager().getPlugin("DataReminder").getDataFolder(), "items.yml");
+        fileForHungerGames = new File(Bukkit.getServer().getPluginManager().getPlugin("DataReminder").getDataFolder(), "itemsHg.yml");
 
         if(!file.exists())
         {
@@ -28,6 +34,20 @@ public class CustomConfig {
                 System.out.println(" Execption :(" + e.getMessage());
             }
         }
+
+        if(!fileForHungerGames.exists())
+        {
+            System.out.println("Trying to Create New File");
+            try
+            {
+                fileForHungerGames.createNewFile();
+            }
+            catch (IOException e)
+            {
+                System.out.println(" Execption :(" + e.getMessage());
+            }
+        }
+
         customFile = YamlConfiguration.loadConfiguration(file);
     }
 
@@ -38,10 +58,23 @@ public class CustomConfig {
         {
             customFile.save(file);
             System.out.println("Saved!");
+            List<String> lines = Files.readAllLines(file.toPath());
+
+            String text = "";
+            for ( String line : lines)
+                text += line + System.lineSeparator();;
+
+            String newtext = text.replaceAll("com.timewars.datarm.classes.myItem",
+                    "com.timewars.hungergames.classes.myItem");
+
+            FileWriter writer = new FileWriter(fileForHungerGames);
+            writer.write(newtext);
+            writer.close();
+            System.out.println("Written to second file!");
         }
         catch (IOException e)
         {
-            System.out.println("Couldn't save CustomFile :(");
+            System.out.println("Couldn't save CustomFile or write to second file :(" + e);
         }
     }
 
